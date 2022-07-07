cb569185fd7006c79324023c7e42fc1f508d4875
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-07 10:38:08 +0900

    versionhenkou

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Switch, Router, Route } from "react-router-dom";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Home } from "./Home";
 import { Page1 } from "./Page1";
@@ -11,18 +11,18 @@ export default function App() {
       <div className="App">
         <Link to="/">Home</Link>
         <br />
-        <Link to="/page1">Page2</Link>
+        <Link to="/page1">Page1</Link>
         <br />
-        <Link to="/page2">Page1</Link>
+        <Link to="/page2">Page2</Link>
       </div>
       <Switch>
-        <Route exact parh="/">
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route exact parh="/page1">
+        <Route path="/page1">
           <Page1 />
         </Route>
-        <Route exact parh="/page2">
+        <Route exact path="/page2">
           <Page2 />
         </Route>
       </Switch>
